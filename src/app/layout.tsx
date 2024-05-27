b9fa29feed46aa707da6dcfc8182347026600d49
Aahmed-Hossain <aahmdctec@gmail.com>
2024-05-28 01:01:18 +0600

    chore: solved the ts errors/warnings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Provider } from "react-redux";
 import { store } from "@/redux/store";
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const inter = Inter({ subsets: ["latin"] });
 
 // export const metadata: Metadata = {
@@ -23,6 +24,22 @@ export default function RootLayout({
     <Provider store={store}>
     {children}
     </Provider>
+
+
+
+
+
+    <ToastContainer 
+position="top-right"
+autoClose={1000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="light" />
         </body>
     </html>
   );
